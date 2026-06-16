7454dd43226059a5edb1a1121af8f9cc8ba8fae3
zoecodes-dev <nrnoonreport@gmail.com>
2026-06-16 18:09:38 +0900

    feat: 프론트엔드 검증함수 제거

--- app/supplier/page.tsx
+++ app/supplier/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import {
   AlertCircle,
@@ -34,7 +34,7 @@ import EightStageStepper from '@/components/supplier/EightStageStepper';
 import SupplyChainMap from '@/components/supplier/SupplyChainMap';
 import ViolationReportModal from '@/components/supplier/ViolationReportModal';
 import AiParsingView from '@/components/supplier/AiParsingView';
-import { getSuppliers, suppliers, supplyEdges } from '@/lib/data';
+import { suppliers, supplyEdges } from '@/lib/data';
 import {
   getCertifications,
   getCompleteness,
@@ -46,10 +46,6 @@ import {
   purchaseOrders,
   regulationMeta,
 } from '@/lib/supplier-detail-data';
-
-useEffect(() => {
-  getSuppliers().then(r => console.log('[연결검증]', r)).catch(e => console.error(e));
-}, []);
 
 const supplierId = 'S-MINE-001';
 
